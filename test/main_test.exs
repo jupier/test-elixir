012dd0c5d85fea3a1b6e6c63ff86ee7cb74d26cb
jpier <jpier@etik.com>
2023-04-30 11:29:55 +0200

    add test for plug router

---
+++ test/main_test.exs
@@ -0,0 +1,28 @@
+defmodule Main.RouterTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  alias Main.Router
+
+  @opts Router.init([])
+
+  test "returns the result table" do
+    conn =
+      :get
+      |> conn("/", "")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
+
+  test "returns 404" do
+    conn =
+      :get
+      |> conn("/missing", "")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+  end
+end
