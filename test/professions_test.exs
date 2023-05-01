263140453b9bf3bae35031708f05d895cda82d13
jpier <jpier@etik.com>
2023-05-01 16:22:37 +0200

    Updating the tests

--- test/professions_test.exs
+++ test/professions_test.exs
@@ -4,10 +4,22 @@ defmodule ProfessionsTest do
 
   @professions ProfessionsService.getProfessions()
 
+  test "Return the profession" do
+    assert Map.get(ProfessionsService.getProfessions(), 17) === "Tech"
+
+    assert Map.get(ProfessionsService.getProfessions(), 25) === "Admin"
+  end
+
   test "Return the profession category from a profession id" do
     assert ProfessionsService.getProfessionCategoryForProfessionId(@professions, 18) === "Tech"
 
     assert ProfessionsService.getProfessionCategoryForProfessionId(@professions, 31) ===
              "Retail"
+  end
+
+  test "Return true if the profession id exists" do
+    assert ProfessionsService.professionIdExists?(@professions, 18) === true
+
+    assert ProfessionsService.professionIdExists?(@professions, 666) === false
   end
 end
